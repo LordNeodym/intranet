889fb5248452f481293bd4e69cf78bb5b6ab73dc
Carlo Voelker <c.voelker@s-v.de>
2015-08-25 18:06:22 +0200

    build register template

--- src/core/forms.py
+++ src/core/forms.py
@@ -2,7 +2,9 @@ from django.contrib.auth.models import User
 from django import forms
 
 class UserForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput())
+    password = forms.CharField(label="Passwort", widget=forms.PasswordInput())
+    password_repeat = forms.CharField(label="Passwort wiederholen", widget=forms.PasswordInput())
+
     class Meta:
         model = User
-        fields = ('username', 'email', 'password')
+        fields = ('username', 'password', 'password_repeat')
--- src/core/views.py
+++ src/core/views.py
@@ -22,12 +22,10 @@ def home(request):
 
 def register(request):
     context = RequestContext(request)
-
     registered = False
 
     if request.method == 'POST':
         user_form = UserForm(data=request.POST)
-        profile_form = UserProfileForm(data=request.POST)
 
         if user_form.is_valid() and profile_form.is_valid():
             user = user_form.save()
@@ -46,10 +44,9 @@ def register(request):
             print user_form.errors, profile_form.errors
     else:
         user_form = UserForm()
-        profile_form = UserProfileForm()
 
     return render_to_response(
-            'rango/register.html', {'user_form': user_form, 'profile_form': profile_form, 'registered': registered}, context)
+            'register.html', {'user_form': user_form, 'registered': registered}, context_instance=context)
 
 
 @never_cache
