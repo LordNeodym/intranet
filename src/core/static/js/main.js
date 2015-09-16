a52031fa4ec579d4f0dda1c64c483f9fb6b29a0c
Carlo Voelker <carlovoelker@gmx.net>
2015-09-16 23:20:07 +0200

    initial tournament brackets

--- src/core/static/js/main.js
+++ src/core/static/js/main.js
@@ -48,3 +48,20 @@ function fancybox() {
 		'overlayShow'	:	false
 	});
 }
+
+
+var minimalData = {
+    teams : [
+      ["Team 1", "Team 2"], /* first matchup */
+      ["Team 3", "Team 4"]  /* second matchup */
+    ],
+    results : [
+      [[1,2], [3,4]],       /* first round */
+      [[4,6], [2,1]]        /* second round */
+    ]
+  }
+ 
+$(function() {
+    $('#tournamen_tree_content').bracket({
+      init: minimalData /* data to initialize the bracket with */ })
+  })
